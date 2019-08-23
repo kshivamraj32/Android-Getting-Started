dbba8bca5239f20f5133b8c2ede8f4f278e5ed3c
Laxmi Kant <laxmimerit@gmail.com>
2019-08-23 09:52:52 +0530

    accel working

--- app/src/main/java/com/example/android_getting_started/MainActivity.java
+++ app/src/main/java/com/example/android_getting_started/MainActivity.java
@@ -7,10 +7,13 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
     private Sensor mLight;
+    private Sensor accel;
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (mLight == null){
+            while (true){
+                Log.d(TAG, "Light Sensor is not present");
+            }
+        }
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
     }
 
     @Override
@@ -31,13 +41,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // The light sensor returns a single value.
         // Many sensors return 3 values, one for each axis.
         float lux = event.values[0];
+
         // Do something with this sensor value.
+        Log.d(TAG, "onSensorChanged: Lux: " + lux);
+
+        Log.d(TAG, "Sensor evenType: " + event.sensor.getType());
+
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            Log.d(TAG, "onSensorChanged: X: " + event.values[0]);
+            Log.d(TAG, "onSensorChanged: Y: " + event.values[1]);
+            Log.d(TAG, "onSensorChanged: Z: " + event.values[2]);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
